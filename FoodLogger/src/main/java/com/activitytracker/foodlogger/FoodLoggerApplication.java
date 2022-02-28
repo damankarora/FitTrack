15844e332465535dffea49830fde8dfe31ce2175
Daman Kumar <damanarora724@gmail.com>
2022-02-28 12:51:40 +0530

    Initialized Foodlogger

---
+++ FoodLogger/src/main/java/com/activitytracker/foodlogger/FoodLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.activitytracker.foodlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodLoggerApplication.class, args);
+    }
+
+}
---
+++ FoodLogger/src/test/java/com/activitytracker/foodlogger/FoodLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitytracker.foodlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
