730361f34a879ac0bfdd339a4e487302c853b357
Daman Kumar <damanarora724@gmail.com>
2022-03-14 15:24:51 +0530

    Created exercise controller in foodlogger

--- FoodLogger/src/main/java/com/activitytracker/foodlogger/Controller/ExerciseController.java
+++ FoodLogger/src/main/java/com/activitytracker/foodlogger/Controller/ExerciseController.java
@@ -1,2 +1,21 @@
-package com.activitytracker.foodlogger.Controller;public class ExerciseController {
+package com.activitytracker.foodlogger.Controller;
+
+import com.activitytracker.foodlogger.ExternalService.ExerciseLogger.TrackExercise;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseController {
+
+    private TrackExercise trackExercise;
+
+    public ExerciseController(TrackExercise trackExercise) {
+        this.trackExercise = trackExercise;
+    }
+
+    @RequestMapping("/exercise")
+    public String callExercise(){
+        return trackExercise.addActivity();
+    }
+
 }
--- FoodLogger/src/main/java/com/activitytracker/foodlogger/ExternalService/ExerciseLogger/TrackExercise.java
+++ FoodLogger/src/main/java/com/activitytracker/foodlogger/ExternalService/ExerciseLogger/TrackExercise.java
@@ -1,2 +1,24 @@
-package com.activitytracker.foodlogger.ExternalService.ExerciseLogger;public class TrackExercise {
+package com.activitytracker.foodlogger.ExternalService.ExerciseLogger;
+
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TrackExercise {
+
+    private final RestTemplate restTemplate;
+
+    public TrackExercise(RestTemplateBuilder restTemplateBuilder) {
+
+            this.restTemplate = restTemplateBuilder.build();
+
+    }
+
+    public String addActivity(){
+        return restTemplate.getForObject("http://localhost:8081/", String.class);
+
+    }
+
 }
